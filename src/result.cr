57119d396376d24dfef6a6a90380143c20c6414f
Alistair A. Israel <aisrael@gmail.com>
2018-03-16 17:32:13 +0800

    Maybe I need to namespace `Result`?

--- src/result.cr
+++ src/result.cr
@@ -2,19 +2,50 @@ abstract struct Result(T)
   def self.success(value : T)
     Success.new(value)
   end
-  def self.failure(message : String)
-    Failure(T).new(message)
+
+  def self.failure(message_or_exception : String | Exception) forall T
+    Failure(T).new(message_or_exception)
   end
+
+  abstract def then(&block : T -> Result(T))
+  abstract def else(&block : -> Result(T))
 end
 
 struct Failure(T) < Result(T)
-  getter :message
-  def initialize(@message : String)
+  def initialize(@message_or_exception : String | Exception)
+  end
+
+  def message : String
+    moe = @message_or_exception
+    return moe if moe.is_a?(String)
+    message = moe.message
+    if !message.nil?
+      message.not_nil!
+    else
+      moe.to_s
+    end
+  end
+
+  def then(&block : T -> Result(T))
+    self
+  end
+
+  def else(&block : -> Result(T))
+    yield
   end
 end
 
 struct Success(T) < Result(T)
   getter :value
+
   def initialize(@value : T)
+  end
+
+  def then(&block : T -> Result(T))
+    yield @value
+  end
+
+  def else(&block : -> Result(T))
+    self
   end
 end
